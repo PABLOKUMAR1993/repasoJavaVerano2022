17fbd9905d531bd057f30b56a6aa48cccf036b60
Pavlo Dudnyk <pablokumar1993@gmail.com>
2022-06-22 23:55:55 +0200

    segundo commit

--- Operadores/src/Ejercicio7.java
+++ Operadores/src/Ejercicio7.java
@@ -2,12 +2,27 @@
 // Construir un programa que, dado un número total de horas, devuelve el número de semanas, días y horas equivalentes.
 // Por ejemplo, dado un total de 1000 horas, debe mostrar 5 semanas, 6 días y 16 horas.
 
+import java.util.Scanner;
+
 public class Ejercicio7 {
 
     public static void main(String args[]) {
 
-        1000 / 24 = 41.6 días.
+        Scanner cantidadHoras;
+        int totalHoras, semanas, dias, horas;
 
+        cantidadHoras = new Scanner(System.in);
+        System.out.println("Introduce la cantidad de horas:");
+        totalHoras = cantidadHoras.nextInt();
+
+        semanas = totalHoras / 168;
+        dias = totalHoras % 168 / 24;
+        horas = totalHoras % 24;
+
+        System.out.println("En las " + totalHoras + " que has indicado, habría un total de:" + "\n" +
+                            semanas + " Semanas." + "\n" +
+                            dias + " Días." + "\n" +
+                            horas + " Horas.");
 
     }
 
---
+++ Operadores/src/Ejercicio8.java
@@ -0,0 +1,5 @@
+// Ejercicio 8.
+// Construir un programa que calcule y muestre por pantalla las raíces de la ecuación de segundo grado de coeficientes reales.
+
+public class Ejercicio8 {
+}
